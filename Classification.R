3461e9360550f454146f68236b317f49664b8c67
Jakub Dmochowski <jakub@dmochowski.co>
2021-01-24 23:46:12 +0100

    Update

---
+++ Classification.R
@@ -0,0 +1,38 @@
+require(ROCR)
+
+#data_train - training data - Required
+#data_test - testing data - Required
+
+data_train_x = data_train %>% select(1:6)
+data_train_y = data_train %>% select(7)
+data_test_x = data_test %>% select(1:6)
+data_test_y = data_test %>% select(7)
+
+svm.model = svm(as.factor(class) ~ ., data = data_train, type = 'C-classification', kernel = 'polynomial', probability = TRUE)
+svm.pred = predict(svm.model, newdata = data_test_x)
+svm.cm = table(data_test_y$class, svm.pred)
+
+nvb.model = naiveBayes(as.factor(class) ~ ., data = data_train)
+nvb.pred = predict(nvb.model, newdata = data_test_x)
+nvb.cm = table(data_test_y$class, nvb.pred)
+
+ct.model = ctree(as.factor(class) ~ ., data = data_train)
+ct.pred = predict(ct.model, newdata = data_test_x)
+ct.cm = table(data_test_y$class, ct.pred)
+
+tibble(svm = f_value(svm.cm), nvb = f_value(nvb.cm), ct = f_value(ct.cm))
+
+svm.prob = predict(svm.model, type="prob", newdata= data_test_x, probability = TRUE)
+svm.rocr = prediction(attr(svm.prob, "probabilities")[,2], data_test_y$class)
+svm.perf = performance(svm.rocr, "tpr","fpr")
+nvb.prob = predict(nvb.model, newdata=data_test_x, type="raw")
+nvb.rocr = prediction(nvb.prob[,2], data_test_y$class)
+nvb.perf = performance(nvb.rocr, "tpr","fpr")
+ct.prob = predict(ct.model, newdata=data_test_x, type="prob")
+ct.rocr = prediction(ct.prob[,2], data_test_y$class)
+ct.perf = performance(ct.rocr, "tpr","fpr")
+plot(svm.perf, col=4, main="ROC curves of different machine learning classifier")
+legend(0.6, 0.6, c('svm', 'ctree', 'Naive Bayes'), 4:6)
+plot(ct.perf, col=5, add=TRUE)
+plot(nvb.perf, col=6, add=TRUE)
+abline(0,1,col="#000000", lty=2)
--- Interface.R
+++ Interface.R
@@ -6,18 +6,25 @@ library(e1071)
 library(dbscan)
 library(mclust)
 library(DDoutlier)
+require(ROCR)
 
 #data_test & data_train - subsets of data, where data_test + data_train = data && data_test inner join data_train = 0
 #data_train = subset of data
 #data_test = subset of data
 
+#Example parameters
+
 #metric = "euclidean"
 #method = "kmeans"
-#args = c("arg1", "arg2")
-#dissimilarity_measure = "distance"
-#outlier_threshold_train = 0.8
-#outlier_threshold_test = 0.8
+#dissimilarity_measure = "CBLOF"
+#outlier_threshold = 0.8
+#algorithm = "Hartigan-Wong"
+#no_clusters = 5
 
+#Dissimilarity measures:
+#CBLOF : (odleg³oœæ od najbli¿szego centroidu) * (rozmiar grupy tego centroidu)
+#uCBLOR: (odleg³oœæ od najbli¿szego centroidu)
+#LDCOF: (odleg³oœæ od najbli¿szego centroidu) / (œrednie odleg³oœci od tego centroidu)
 
 # dissimilarity measures should later include LOF, CBLOF, uCBLOF, LDCOF
 # allowed metrics depends on used method
@@ -26,6 +33,28 @@ library(DDoutlier)
 #args for dbscan: 1 - scan_distance, 2 - fraction_of_outliers
 
 
+f_value <- function (cm) {
+  fp = cm[1,2]
+  fn = cm[2,1]
+  tp = cm[2,2]
+  recall = tp / (tp+fn)
+  precision = tp / (tp+fp)
+  f = (2 * recall * precision) / (recall + precision)
+  return(f)
+}
+
+
+predict.kmeans <- function(object, newdata, metric){
+  centers <- object$centers
+  n_centers <- nrow(centers)
+  dist_mat <- as.matrix(dist(rbind(centers, newdata), method=metric))
+  dist_mat <- dist_mat[-seq(n_centers), seq(n_centers)]
+  max.col(-dist_mat)
+}
+
+normalize_outlier_score <- function (data) {
+  data$outlier_score / max(data$outlier_score)
+}
 
 ## @knitr nazwachunka
 
@@ -35,11 +64,20 @@ data_test_x = data_test %>% select(1:6)
 data_test_y = data_test %>% select(7)
 
 show_stats <- function(data){
+  #prepare data
+  data$outlier_score = normalize_outlier_score(data)
+  data = data %>% mutate(pred = outlier_score)
+  data = data %>% mutate(pred_int = as.integer(outlier_score > outlier_threshold))
+  data = data %>% arrange(outlier_score)
+
+  #dissimilarity measure distribution
+  plot(data$outlier_score, main="dissimilarity distribution")
+  
   #roc curve
   pred = prediction(predictions=data$pred, labels=data$class)
   perf = performance(pred, "tpr", "fpr")
   plot(perf, main="ROC curve")
-  abline(0,1, col="#0000ff")
+  abline(0,1,col="#000000", lty=2)
   
   #f1-measure
   data = data %>% mutate(tp = class & pred_int)
@@ -51,61 +89,59 @@ show_stats <- function(data){
   f_value = 2 * recall * precision / (recall + precision)
   print(paste("F-value:", f_value))
 }
-
 if(method == "kmeans") {
   
-  cluster_data = kmeans(data_train_x, as.integer(args[1]), algorithm=args[2])
+  cluster_data = kmeans(data_train_x, no_clusters, algorithm=algorithm)
   plot(fviz_cluster(cluster_data, data = data_train_x))
-  if(dissimilarity_measure == "distance") {
-    cluster_centers = cluster_data$centers
-    data = data_train
-    data %<>% mutate(group = cluster_data$cluster) %>% mutate(dist = sqrt((V1-cluster_centers[group, 1])^2 + (V2-cluster_centers[group, 2])^2 + (V3-cluster_centers[group, 3])^2 + (V4-cluster_centers[group, 4])^2)) %>% arrange(dist)
-    data$dist = data$dist / max(data$dist)
-    data = data %>% mutate(pred = dist)
-    data = data %>% mutate(pred_int = as.integer(dist > outlier_threshold_train))
-    plot(data$dist, main="train set dissimilarity distribution")
-    
-    predict.kmeans <- function(object, newdata){
-      centers <- object$centers
-      n_centers <- nrow(centers)
-      dist_mat <- as.matrix(dist(rbind(centers, newdata)))
-      dist_mat <- dist_mat[-seq(n_centers), seq(n_centers)]
-      max.col(-dist_mat)
-    }
-    
-    groups = predict(cluster_data, data_test_x)
-    data = data_test %>% mutate(group = groups)
-    data %<>% mutate(dist = sqrt((V1-cluster_centers[group, 1])^2 + (V2-cluster_centers[group, 2])^2 + (V3-cluster_centers[group, 3])^2 + (V4-cluster_centers[group, 4])^2)) %>% arrange(dist)
-    data$dist = data$dist / max(data$dist)
-    data = data %>% mutate(pred = dist)
-    data = data %>% mutate(pred_int = as.integer(dist > outlier_threshold_test))
-    plot(data$dist, main="test set dissimilarity distribution")
-    
-    show_stats(data)
-  }
+  groups = predict.kmeans(cluster_data, data_test_x, metric)
+  data = data_test %>% mutate(group = groups)
   
-} else if(method == "dbscan") {
+} else if(method == "cmeans") {
   
-  diss_matrix = dist(data_train_x, method=metric)
-  cluster_data = dbscan(diss_matrix, eps=as.numeric(args[1]))
-  plot(
-    fviz_cluster(
-      list(data = data_train_x, 
-      cluster=cluster_data$cluster),
-      ggtheme = theme_minimal()
-    )
-  )
-  
+  cluster_data = cmeans(data_train_x, no_clusters, dist=metric)
+  plot(fviz_cluster(list(data = data_train_x, cluster=cluster_data$cluster), ggtheme = theme_minimal()))
   data = data_test
+  centers = cluster_data$centers
+  data$group = cmeans(data_test_x, 4, dist=metric)$cluster
+}
+
+cluster_centers = cluster_data$centers
+
+if(dissimilarity_measure == "uCBLOF") {
   
-  if(dissimilarity_measure == 'DB') {
-    diss = data %>% select(1:6)
-    data$pred = DB(diss, d = as.numeric(args[1]), fraction = as.numeric(args[2]))$classification
-    data = data %>% mutate(pred = as.integer(pred == 'Outlier'))
-    data = data %>% mutate(pred_int = as.integer(pred == 'Outlier'))
-  }
+  data = data %>% mutate(outlier_score = sqrt((V1-cluster_centers[group, 1])^2 + (V2-cluster_centers[group, 2])^2 + (V3-cluster_centers[group, 3])^2 + (V4-cluster_centers[group, 4])^2))
   
+} else if(dissimilarity_measure == "LDCOF") {
   
-  show_stats(data)
+  data = data %>% mutate(dist = sqrt((V1-cluster_centers[group, 1])^2 + (V2-cluster_centers[group, 2])^2 + (V3-cluster_centers[group, 3])^2 + (V4-cluster_centers[group, 4])^2))
+  average_dist = data %>% group_by(group) %>% summarise(avg_dist = mean(dist))
+  data = data %>% mutate(outlier_score = dist / average_dist[group,2]$avg_dist)
   
-} 
+} else if(dissimilarity_measure == "CBLOF") {
+  
+  data = data %>% mutate(dist = sqrt((V1-cluster_centers[group, 1])^2 + (V2-cluster_centers[group, 2])^2 + (V3-cluster_centers[group, 3])^2 + (V4-cluster_centers[group, 4])^2))
+  group_count = data %>% group_by(group) %>% summarise(count = n())
+  data = data %>% mutate(outlier_score = dist * group_count[group,2]$count)
+  
+}
+show_stats(data)
+
+#} else if(method == "dbscan") {
+#  diss_matrix = dist(data_train_x, method=metric)
+#  cluster_data = dbscan(diss_matrix, eps=as.numeric(args[1]))
+#  plot(
+#    fviz_cluster(
+#      list(data = data_train_x, 
+#      cluster=cluster_data$cluster),
+#      ggtheme = theme_minimal()
+#    )
+#  )
+#  data = data_test
+#  if(dissimilarity_measure == 'DB') {
+#    diss = data %>% select(1:6)
+#    data$pred = DB(diss, d = as.numeric(args[1]), fraction = as.numeric(args[2]))$classification
+#    data = data %>% mutate(pred = as.integer(pred == 'Outlier'))
+#    data = data %>% mutate(pred_int = as.integer(pred == 'Outlier'))
+#  }
+#  show_stats(data)
+#} 
